8fe1299cf67ce807a3f8a76d675d3633014befb5
ZeZar6 <cesarrodriguezgutierrez20@gmail.com>
2025-08-11 11:06:10 +0200

    Actualización de código

--- project4.py
+++ project4.py
@@ -19,6 +19,9 @@ col1, col2= st.columns([1,1])
 selected_port = col1.selectbox('Select Port of Embarkation', embarked_port)
 selected_gender = col2.selectbox('Select Gender', gender)
 
+df_plot= df[df['Embarked'] == selected_port]
+df_plot= df_plot[df_plot['Sex']==selected_gender]
 
+plot= px.histogram(df_plot, x='Age', color='Survived', template='seaborn', title='Survival by Age', facet_col='Survived')
 
-
+st.plotly_chart(plot)
